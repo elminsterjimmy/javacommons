3b9a008d109952d97ce451c0f592960ffb37b631
JGu <Jinglei.Gu@kisters.cn>
2015-09-24 18:02:28 +0800

    + remove unused improts.

--- spring/security/src/main/java/com/elminster/spring/security/filter/CsrfTokenFilter.java
+++ spring/security/src/main/java/com/elminster/spring/security/filter/CsrfTokenFilter.java
@@ -8,7 +8,6 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.access.AccessDeniedHandlerImpl;
