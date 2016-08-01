59a2e16f9228997495e1aeb12d56d29fd7faaf9f
Jinglei Gu <jinglei.gu@oracle.com>
2016-08-01 17:16:16 +0800

    Add error log to NoneUncatchedExceptionHandler to make the exception traceable.

--- commons/src/main/java/com/elminster/common/thread/NoneUncatchedExceptionHandler.java
+++ commons/src/main/java/com/elminster/common/thread/NoneUncatchedExceptionHandler.java
@@ -1,9 +1,16 @@
 package com.elminster.common.thread;
 
-public class NoneUncatchedExceptionHandler implements UncatchedExceptionHandler {
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
+import com.elminster.common.util.ExceptionUtil;
+
+public class NoneUncatchedExceptionHandler implements UncatchedExceptionHandler {
+  
+  private static final Log logger = LogFactory.getLog(NoneUncatchedExceptionHandler.class);
+  
   @Override
   public void handleUncatchedException(Throwable t) {
-    
+    logger.error(ExceptionUtil.getCause(t));
   }
 }
