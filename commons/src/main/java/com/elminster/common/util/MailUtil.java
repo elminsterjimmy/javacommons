bde1d361f90e399225c21eff65d651451932cbee
Jinglei.Gu <jinglei.gu@oracle.com>
2016-09-08 14:43:07 +0800

    enhance mail utilities with NPE checks and sending mail via localhost.

--- commons/src/main/java/com/elminster/common/util/MailUtil.java
+++ commons/src/main/java/com/elminster/common/util/MailUtil.java
@@ -1,6 +1,7 @@
 package com.elminster.common.util;
 
 import java.io.File;
+import java.util.Date;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Properties;
@@ -68,7 +69,7 @@ abstract public class MailUtil {
    * @throws AddressException
    * @throws MessagingException
    */
-  public static boolean sendMail(String smtpHost, int smtpPort, String user, String password, boolean isAutheticate,
+  public static boolean sendMail(String smtpHost, Integer smtpPort, String user, String password, boolean isAutheticate,
       String from, String to, String cc, String bcc, String subject, String content, Map<String, String> filePathMap)
       throws AddressException, MessagingException {
 
@@ -77,17 +78,23 @@ abstract public class MailUtil {
     p.put("mail.smtp.auth", isAutheticate ? Boolean.TRUE.toString() : Boolean.FALSE.toString());
     p.put("mail.transport.protocol", DEFAULT_SEND_PROTOCOL);
     p.put("mail.smtp.host", smtpHost);
-    p.put("mail.smtp.port", smtpPort);
-    Session session = Session.getInstance(p);
+    if (null != smtpPort) {
+      p.put("mail.smtp.port", smtpPort);
+    }
+    Session session = Session.getDefaultInstance(p);
     Message msg = new MimeMessage(session);
     // From
     msg.setFrom(new InternetAddress(from));
     // TO
     msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
-    msg.setRecipients(Message.RecipientType.CC, InternetAddress.parse(cc));
-    msg.setRecipients(Message.RecipientType.BCC, InternetAddress.parse(bcc));
+    if (null != cc) {
+      msg.setRecipients(Message.RecipientType.CC, InternetAddress.parse(cc));
+    }
+    if (null != bcc) {
+      msg.setRecipients(Message.RecipientType.BCC, InternetAddress.parse(bcc));
+    }
     // sent date
-    msg.setSentDate(DateUtil.getCurrentDate());
+    msg.setSentDate(new Date(System.currentTimeMillis()));
     // subject
     msg.setSubject(subject);
 
@@ -101,14 +108,14 @@ abstract public class MailUtil {
     String fileName;
     String fileFullPath;
     DataSource source;
-    if (CollectionUtil.isNotEmpty(filePathMap)) {
+    if (null != filePathMap && !filePathMap.isEmpty()) {
       Iterator<Map.Entry<String, String>> it = filePathMap.entrySet().iterator();
       while (it.hasNext()) {
         mbp = new MimeBodyPart();
         Map.Entry<String, String> entry = (Map.Entry<String, String>) it.next();
         fileName = entry.getKey();
         fileFullPath = entry.getValue();
-        if (StringUtil.isEmpty(fileFullPath)) {
+        if (null == fileFullPath || "".equals(fileFullPath)) {
           continue;
         }
         File f = new File(fileFullPath);
@@ -123,12 +130,42 @@ abstract public class MailUtil {
       }
     }
     msg.setContent(mulp);
-    // log in to server
-    Transport tran = session.getTransport(DEFAULT_SEND_PROTOCOL);
-    tran.connect(smtpHost, user, password);
-
-    tran.sendMessage(msg, msg.getAllRecipients());
+    Transport tran = null;
+    if (null != user) {
+      // log in to server
+      tran = session.getTransport(DEFAULT_SEND_PROTOCOL);
+      tran.connect(smtpHost, user, password);
+      tran.sendMessage(msg, msg.getAllRecipients());
+    } else {
+      Transport.send(msg, msg.getAllRecipients());
+    }
     rst = true;
     return rst;
+  }
+  
+  /**
+   * Send mail via localhost mail service.
+   * 
+   * @param from
+   *          mail from
+   * @param to
+   *          mail to
+   * @param cc
+   *          mail cc
+   * @param bcc
+   *          mail bcc
+   * @param subject
+   *          subject of mail
+   * @param content
+   *          content of mail
+   * @param filePathMap
+   *          attachment file path map
+   * @return send mail success or not
+   * @throws AddressException
+   * @throws MessagingException
+   */
+  public static boolean sendMailViaLocalhost(String from, String to, String cc, String bcc, String subject,
+      String content, Map<String, String> filePathMap) throws AddressException, MessagingException {
+    return sendMail("localhost", null, null, null, false, from, to, cc, bcc, subject, content, filePathMap);
   }
 }
