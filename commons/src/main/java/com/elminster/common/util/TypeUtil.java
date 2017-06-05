8e2eed838ecd5d7a1cde25713b052fdbf889b8db
Jinglei.Gu <jinglei.gu@oracle.com>
2017-06-05 10:26:37 +0800

    refactor TypeUtil.

--- commons/src/main/java/com/elminster/common/util/TypeUtil.java
+++ commons/src/main/java/com/elminster/common/util/TypeUtil.java
@@ -6,148 +6,103 @@ import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 import java.lang.reflect.TypeVariable;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.StringJoiner;
+import java.util.concurrent.ConcurrentHashMap;
 
-public class TypeUtil {
+import com.elminster.common.constants.Constants.StringConstants;
 
-  public static final TypeUtil INSTANCE = new TypeUtil();
+/**
+ * Type Utilities.
+ * 
+ * @author jinggu
+ * @version 1.0
+ */
+abstract public class TypeUtil {
 
-  private final Map<Class<?>, ClassTypeDef[]> classCache = new HashMap<Class<?>, ClassTypeDef[]>();
+  /** cache the class type information. */
+  private final static Map<Class<?>, ClassTypeDef[]> classCache = new ConcurrentHashMap<>();
 
-  private TypeUtil() {
-  }
-
-  public void clearCache() {
+  public static void clearCache() {
     classCache.clear();
   }
-  
+
   /**
    * Get the method generic parameter type class.
-   * @param clazz the class of the method
-   * @param method the method
-   * @param idx the parameter index
+   * 
+   * @param clazz
+   *          the class of the method
+   * @param method
+   *          the method
+   * @param idx
+   *          the parameter index
    * @return the method generic parameter type class
    */
-  public List<Class<?>> getMethodParameterTypeClass(Class<?> clazz, Method method, int idx) {
-    ClassTypeDef[] defs = classCache.get(clazz);
-    if (null == defs) {
-      defs = getClassGenericTypesClass(clazz);
-    }
-    
-    List<Class<?>> actualClass = null;
+  public static CompactedType getMethodParameterTypeClass(Class<?> clazz, Method method, int idx) {
+    CompactedType ct = null;
     Type type = method.getGenericParameterTypes()[idx];
     if (type instanceof Class<?>) {
-      actualClass = new ArrayList<Class<?>>(0);
-      actualClass.add((Class<?>) type);
+      ct = new CompactedType();
+      ct.type = (Class<?>) type;
+      return ct;
     } else if (type instanceof TypeVariable) {
       TypeVariable<?> tv = (TypeVariable<?>) type;
-      actualClass = getActualClassFromTypeVariable(tv, defs);
-      while (null == actualClass && null != clazz.getSuperclass()) {
+      ct = getActualClassFromTypeVariableReadable(tv, clazz);
+      while (null == ct && null != clazz.getSuperclass()) {
         clazz = clazz.getSuperclass();
-        actualClass = getMethodReturnTypeClass(clazz, method);
+        ct = getMethodParameterTypeClass(clazz, method, idx);
       }
     } else if (type instanceof ParameterizedType) {
       ParameterizedType pt = (ParameterizedType) type;
-      actualClass = getTypeFromParameterizedType(pt, defs);
+      ct = getTypeFromParameterizedType(pt, clazz);
     }
-    return actualClass;
+    return ct;
   }
 
   /**
    * Get the method generic return type class.
-   * @param clazz the class of the method
-   * @param method the method
+   * 
+   * @param clazz
+   *          the class of the method
+   * @param method
+   *          the method
    * @return the method generic return type class
    */
-  public List<Class<?>> getMethodReturnTypeClass(Class<?> clazz, Method method) {
-    ClassTypeDef[] defs = classCache.get(clazz);
-    if (null == defs) {
-      defs = getClassGenericTypesClass(clazz);
-    }
-
-    List<Class<?>> actualClass = null;
+  public static CompactedType getMethodReturnTypeClass(Class<?> clazz, Method method) {
+    CompactedType ct = null;
     Type type = method.getGenericReturnType();
     if (type instanceof Class<?>) {
-      actualClass = new ArrayList<Class<?>>(0);
-      actualClass.add((Class<?>) type);
+      ct = new CompactedType();
+      ct.type = (Class<?>) type;
+      return ct;
     } else if (type instanceof TypeVariable) {
       TypeVariable<?> tv = (TypeVariable<?>) type;
-      actualClass = getActualClassFromTypeVariable(tv, defs);
-      while (null == actualClass && null != clazz.getSuperclass()) {
+      ct = getActualClassFromTypeVariableReadable(tv, clazz);
+      while (null == ct && null != clazz.getSuperclass()) {
         clazz = clazz.getSuperclass();
-        actualClass = getMethodReturnTypeClass(clazz, method);
+        ct = getMethodReturnTypeClass(clazz, method);
       }
     } else if (type instanceof ParameterizedType) {
       ParameterizedType pt = (ParameterizedType) type;
-      actualClass = getTypeFromParameterizedType(pt, defs);
+      ct = getTypeFromParameterizedType(pt, clazz);
     }
-    return actualClass;
+    return ct;
   }
 
-  /**
-   * Get the field generic type class.
-   * @param clazz the class of the field
-   * @param field the field
-   * @return the actual class of the generic type
-   */
-  public List<Class<?>> getFieldGenericTypeClass(Class<?> clazz, Field field) {
-    ClassTypeDef[] defs = classCache.get(clazz);
+  private static CompactedType getActualClassFromTypeVariableReadable(TypeVariable<?> tv, Class<?> clazz) {
+    ClassTypeDef[] defs = TypeUtil.classCache.get(clazz);
     if (null == defs) {
       defs = getClassGenericTypesClass(clazz);
     }
-
-    List<Class<?>> actualClass = null;
-    Type type = field.getGenericType();
-    if (type instanceof Class<?>) {
-      actualClass = new ArrayList<Class<?>>(0);
-      actualClass.add((Class<?>) type);
-    } else if (type instanceof TypeVariable) {
-      TypeVariable<?> tv = (TypeVariable<?>) type;
-      actualClass = getActualClassFromTypeVariable(tv, defs);
-      while (null == actualClass && null != clazz.getSuperclass()) {
-        clazz = clazz.getSuperclass();
-        actualClass = getFieldGenericTypeClass(clazz, field);
-      }
-    } else if (type instanceof ParameterizedType) {
-      ParameterizedType pt = (ParameterizedType) type;
-      actualClass = getTypeFromParameterizedType(pt, defs);
-    }
-    return actualClass;
-  }
-
-  private List<Class<?>> getActualClassFromTypeVariable(TypeVariable<?> tv, ClassTypeDef[] defs) {
     if (null != defs) {
       for (ClassTypeDef def : defs) {
         if (tv.getName().equals(def.getTypeVariableName()) && tv.getGenericDeclaration() == def.getTypeDefClass()) {
-          return def.getActualClass();
+          return def.ct;
         }
       }
     }
     return null;
-  }
-
-  private List<Class<?>> getTypeFromParameterizedType(ParameterizedType pt, ClassTypeDef[] defs) {
-    List<Class<?>> rtn = null;
-    Type rawType = pt.getRawType();
-    if (rawType instanceof Class<?>) {
-      rtn = new ArrayList<Class<?>>();
-      Type[] types = pt.getActualTypeArguments();
-      for (Type type :types) {
-        if (type instanceof TypeVariable<?>) {
-          rtn.addAll(getActualClassFromTypeVariable((TypeVariable<?>)type, defs));
-        } else if (type instanceof ParameterizedType) {
-          rtn.add((Class<?>) rawType);
-          rtn.addAll(getTypeFromParameterizedType((ParameterizedType) type, defs));
-        } else if (type instanceof Class<?>) {
-          rtn.add((Class<?>) rawType);
-          rtn.add((Class<?>) type);
-        }
-      }
-      
-    }
-    return rtn;
   }
 
   /**
@@ -157,31 +112,30 @@ public class TypeUtil {
    *          the class
    * @return the generic types actual classes
    */
-  public ClassTypeDef[] getClassGenericTypesClass(Class<?> clazz) {
+  public static ClassTypeDef[] getClassGenericTypesClass(Class<?> clazz) {
     Type genericSuperclass = clazz.getGenericSuperclass();
     ClassTypeDef[] superClassGenericClass = null;
     ClassTypeDef[] interfaceGenericClass = null;
     if (null != genericSuperclass) {
-      superClassGenericClass = getClassGenericDef(genericSuperclass);
+      superClassGenericClass = getClassGenericDef(genericSuperclass, clazz);
     }
 
     Type[] genericInterfaces = clazz.getGenericInterfaces();
     if (null != genericInterfaces) {
       for (Type genericInterface : genericInterfaces) {
-        ClassTypeDef[] ifgc = getClassGenericDef(genericInterface);
+        ClassTypeDef[] ifgc = getClassGenericDef(genericInterface, clazz);
         interfaceGenericClass = (ClassTypeDef[]) ArrayUtil.joinArray(interfaceGenericClass, ifgc);
       }
     }
-    ClassTypeDef[] classTypeClass = (ClassTypeDef[]) ArrayUtil
-        .joinArray(superClassGenericClass, interfaceGenericClass);
+    ClassTypeDef[] classTypeClass = (ClassTypeDef[]) ArrayUtil.joinArray(superClassGenericClass, interfaceGenericClass);
     classCache.put(clazz, classTypeClass);
     return classTypeClass;
   }
 
-  private ClassTypeDef[] getClassGenericDef(Type type) {
+  private static ClassTypeDef[] getClassGenericDef(Type type, Class<?> clazz) {
     ClassTypeDef[] classTypeDef = null;
     if (type instanceof ParameterizedType) {
-      classTypeDef = getClassTypeDefFromParameterizedType((ParameterizedType) type);
+      classTypeDef = getClassTypeDefFromParameterizedType((ParameterizedType) type, clazz);
     }
     return classTypeDef;
   }
@@ -191,7 +145,7 @@ public class TypeUtil {
    * @param parameterizedType
    * @return
    */
-  private ClassTypeDef[] getClassTypeDefFromParameterizedType(ParameterizedType parameterizedType) {
+  private static ClassTypeDef[] getClassTypeDefFromParameterizedType(ParameterizedType parameterizedType, Class<?> clazz) {
     ClassTypeDef[] classTypeDefs = null;
     Type rawType = parameterizedType.getRawType();
     if (rawType instanceof Class<?>) {
@@ -206,6 +160,7 @@ public class TypeUtil {
           ctd.setTypeDefClass(c);
           ctd.setTypeVariableName(tv[i].getName());
           ctd.setActualClass(getClassFromRawType(type));
+          ctd.ct = getCompactedTypeFromRawType(type, clazz);
           classTypeDefs[i++] = ctd;
         }
       }
@@ -213,12 +168,7 @@ public class TypeUtil {
     return classTypeDefs;
   }
 
-  /**
-   * 
-   * @param type
-   * @return
-   */
-  private List<Class<?>> getClassFromRawType(Type type) {
+  private static List<Class<?>> getClassFromRawType(Type type) {
     List<Class<?>> list = null;
     if (type instanceof Class<?>) {
       list = new ArrayList<Class<?>>(0);
@@ -236,11 +186,133 @@ public class TypeUtil {
     return list;
   }
 
-  public class ClassTypeDef {
+  private static CompactedType getCompactedTypeFromRawType(Type type, Class<?> clazz) {
+    CompactedType rtn = null;
+    if (type instanceof Class<?>) {
+      rtn = new CompactedType();
+      rtn.type = (Class<?>) type;
+    } else if (type instanceof ParameterizedType) {
+      ParameterizedType pt = (ParameterizedType) type;
+      Type rawType = pt.getRawType();
+      if (rawType instanceof Class<?>) {
+        Type[] types = pt.getActualTypeArguments();
+        rtn = new CompactedType();
+        rtn.type = (Class<?>) rawType;
+        for (Type t : types) {
+          if (t instanceof TypeVariable<?>) {
+            rtn.children.add(getActualClassFromTypeVariableReadable((TypeVariable<?>) type, clazz));
+          } else if (t instanceof ParameterizedType) {
+            rtn.children.add(getCompactedTypeFromRawType((ParameterizedType) t, clazz));
+          } else if (t instanceof Class<?>) {
+            CompactedType ct = new CompactedType();
+            ct.type = (Class<?>) t;
+            rtn.children.add(ct);
+          }
+        }
+      } else {
+        // TODO
+        throw new UnsupportedOperationException(String.format("unsupported rawType [%s].", rawType));
+      }
+    }
+    return rtn;
+  }
+
+  public static boolean isPrimitive(Class<?> clazz) {
+    if (clazz.isArray()) {
+      return isPrimitive(clazz.getComponentType());
+    }
+    return clazz.isPrimitive();
+  }
+
+  /**
+   * Get the field generic type class.
+   * 
+   * @param clazz
+   *          the class of the field
+   * @param field
+   *          the field
+   * @return the actual class of the generic type
+   */
+  public static CompactedType getFieldGenericTypeClass(Class<?> clazz, Field field) {
+    CompactedType ct = null;
+    Type type = field.getGenericType();
+    if (type instanceof Class<?>) {
+      ct = new CompactedType();
+      ct.type = (Class<?>) type;
+      return ct;
+    } else if (type instanceof TypeVariable) {
+      TypeVariable<?> tv = (TypeVariable<?>) type;
+      ct = getActualClassFromTypeVariableReadable(tv, clazz);
+      while (null == ct && null != clazz.getSuperclass()) {
+        clazz = clazz.getSuperclass();
+        ct = getFieldGenericTypeClass(clazz, field);
+      }
+    } else if (type instanceof ParameterizedType) {
+      ParameterizedType pt = (ParameterizedType) type;
+      ct = getTypeFromParameterizedType(pt, clazz);
+    }
+    return ct;
+  }
+
+  private static CompactedType getTypeFromParameterizedType(ParameterizedType pt, Class<?> clazz) {
+    CompactedType rtn = null;
+    Type rawType = pt.getRawType();
+    if (rawType instanceof Class<?>) {
+      rtn = new CompactedType();
+      rtn.type = (Class<?>) rawType;
+      Type[] types = pt.getActualTypeArguments();
+      for (Type type : types) {
+        if (type instanceof TypeVariable<?>) {
+          rtn.children.add(getActualClassFromTypeVariableReadable((TypeVariable<?>) type, clazz));
+        } else if (type instanceof ParameterizedType) {
+          rtn.children.add(getTypeFromParameterizedType((ParameterizedType) type, clazz));
+        } else if (type instanceof Class<?>) {
+          CompactedType ct = new CompactedType();
+          ct.type = (Class<?>) type;
+          rtn.children.add(ct);
+        }
+      }
+
+    }
+    return rtn;
+  }
+
+  public static class CompactedType {
+    private static final String CHILD_JOIN_CHAR = StringConstants.COMMA + StringConstants.SPACE;
+    private static final String TYPE_START = StringConstants.LESS_THAN;
+    private static final String TYPE_END = StringConstants.GREAT_THAN;
+    
+    Class<?> type;
+    List<CompactedType> children = new ArrayList<>();
+
+    public String toString(boolean simple) {
+      StringBuilder sb = new StringBuilder();
+
+      sb.append(simple ? type.getSimpleName() : type.getName());
+      if (!children.isEmpty()) {
+        sb.append(TYPE_START);
+        StringJoiner joiner = new StringJoiner(CHILD_JOIN_CHAR);
+        
+        for (CompactedType child : children) {
+          joiner.add(child.toString(simple));
+        }
+        sb.append(joiner.toString());
+        sb.append(TYPE_END);
+      }
+      return sb.toString();
+    }
+
+    public String toString() {
+      return toString(false);
+    }
+  }
+
+  public static class ClassTypeDef {
     Class<?> typeDefClass;
     String typeVariableName;
     // the actual class also can be a ParameterizedType
     List<Class<?>> actualClass;
+    CompactedType ct;
 
     public ClassTypeDef() {
 
--- commons/src/test/java/com/elminster/common/util/test/TypeUtilTest.java
+++ commons/src/test/java/com/elminster/common/util/test/TypeUtilTest.java
@@ -6,11 +6,11 @@ import java.util.List;
 import java.util.Map;
 
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import com.elminster.common.util.ReflectUtil;
 import com.elminster.common.util.TypeUtil;
+import com.elminster.common.util.TypeUtil.CompactedType;
 import com.elminster.common.util.TypeUtil.ClassTypeDef;
 
 public class TypeUtilTest {
@@ -18,7 +18,7 @@ public class TypeUtilTest {
   @Test
   public void testGetClassGenericTypesClasses() {
     Class<?> clazz = CT3.class;
-    ClassTypeDef[] classTypeDef = TypeUtil.INSTANCE.getClassGenericTypesClass(clazz);
+    ClassTypeDef[] classTypeDef = TypeUtil.getClassGenericTypesClass(clazz);
     Assert.assertTrue(3 == classTypeDef.length);
     Assert.assertTrue(Short.class == classTypeDef[0].getActualClass().get(1));
     Assert.assertTrue(CT2.class == classTypeDef[0].getTypeDefClass());
@@ -33,27 +33,46 @@ public class TypeUtilTest {
   
   @Test
   public void testgetFieldTypeClass() {
-    Class<?> clazz = CT3.class;
+    Class<?> clazz = GT.class;
+    Field fieldclist = ReflectUtil.getDeclaredField(clazz, "clist");
+    Field fieldtriMap = ReflectUtil.getDeclaredField(clazz, "triMap");
+    CompactedType ctclist = TypeUtil.getFieldGenericTypeClass(clazz, fieldclist);
+    CompactedType cttriMap = TypeUtil.getFieldGenericTypeClass(clazz, fieldtriMap);
+    
+    clazz = CT3.class;
     Field fieldT = ReflectUtil.getDeclaredField(clazz, "t");
     Field fieldK = ReflectUtil.getDeclaredField(clazz, "k");
     Field fieldLK = ReflectUtil.getDeclaredField(clazz, "lk");
     Field fieldLIT = ReflectUtil.getDeclaredField(clazz, "lit");
     Field fieldLS = ReflectUtil.getDeclaredField(clazz, "ls");
-    Assert.assertTrue(Long.class == TypeUtil.INSTANCE.getFieldGenericTypeClass(clazz, fieldT).get(0));
-    Assert.assertTrue(List.class == TypeUtil.INSTANCE.getFieldGenericTypeClass(clazz, fieldK).get(0));
-    List<Class<?>> TypeClassLK = TypeUtil.INSTANCE.getFieldGenericTypeClass(clazz, fieldLK);
-    Assert.assertTrue(List.class == TypeClassLK.get(0));
-    Assert.assertTrue(Short.class == TypeClassLK.get(1));
-    List<Class<?>> TypeClassLIT = TypeUtil.INSTANCE.getFieldGenericTypeClass(clazz, fieldLIT);
-    Assert.assertTrue(List.class == TypeClassLIT.get(0));
-    Assert.assertTrue(IT1.class == TypeClassLIT.get(1));
-    Assert.assertTrue(String.class == TypeClassLIT.get(2));
-    List<Class<?>> TypeClassLS = TypeUtil.INSTANCE.getFieldGenericTypeClass(clazz, fieldLS);
-    Assert.assertTrue(List.class == TypeClassLS.get(0));
-    Assert.assertTrue(String.class == TypeClassLS.get(1));
+    CompactedType ctT = TypeUtil.getFieldGenericTypeClass(clazz, fieldT);
+    CompactedType ctK = TypeUtil.getFieldGenericTypeClass(clazz, fieldK);
+    CompactedType ctLK = TypeUtil.getFieldGenericTypeClass(clazz, fieldLK);
+    CompactedType ctLIT = TypeUtil.getFieldGenericTypeClass(clazz, fieldLIT);
+    CompactedType ctLS = TypeUtil.getFieldGenericTypeClass(clazz, fieldLS);
+    // List<List<Map<String, Map<String, String>>>>
+    Assert.assertEquals("java.util.List<java.util.List<java.util.Map<java.lang.String, java.util.Map<java.lang.String, java.lang.String>>>>", ctclist.toString());
+    Assert.assertEquals("List<List<Map<String, Map<String, String>>>>", ctclist.toString(true));
+    // Map<String, Map<String, Map<String, String>>>
+    Assert.assertEquals("java.util.Map<java.lang.String, java.util.Map<java.lang.String, java.util.Map<java.lang.String, java.lang.String>>>", cttriMap.toString());
+    Assert.assertEquals("Map<String, Map<String, Map<String, String>>>", cttriMap.toString(true));
+    // Long
+    Assert.assertEquals("java.lang.Long", ctT.toString());
+    Assert.assertEquals("Long", ctT.toString(true));
+    // K -> List<Short>
+    Assert.assertEquals("java.util.List<java.lang.Short>", ctK.toString());
+    Assert.assertEquals("List<Short>", ctK.toString(true));
+    // List<K> -> List<List<Short>>
+    Assert.assertEquals("java.util.List<java.util.List<java.lang.Short>>", ctLK.toString());
+    Assert.assertEquals("List<List<Short>>", ctLK.toString(true));
+    // List<IT1<String>>
+    Assert.assertEquals("java.util.List<com.elminster.common.util.test.TypeUtilTest$IT1<java.lang.String>>", ctLIT.toString());
+    Assert.assertEquals("List<IT1<String>>", ctLIT.toString(true));
+    // List<String>
+    Assert.assertEquals("java.util.List<java.lang.String>", ctLS.toString());
+    Assert.assertEquals("List<String>", ctLS.toString(true));
   }
   
-  @Ignore
   @Test
   public void testGetMethodReturnTypeClass() {
     Class<?> clazz = CT3.class;
@@ -62,16 +81,13 @@ public class TypeUtilTest {
     Method methodIT2 = ReflectUtil.getDeclaredMethod(clazz, "getIT2", List.class, Integer.class);
     Method methodCT1 = ReflectUtil.getDeclaredMethod(clazz, "getCT1", Long.class);
     Method methodCT2 = ReflectUtil.getDeclaredMethod(clazz, "getCT2", String.class, List.class);
-    Assert.assertTrue(String.class == TypeUtil.INSTANCE.getMethodReturnTypeClass(clazz, methodString).get(0));
-    Assert.assertTrue(Object.class == TypeUtil.INSTANCE.getMethodReturnTypeClass(clazz, methodIT1).get(0));
-    Assert.assertTrue(List.class == TypeUtil.INSTANCE.getMethodReturnTypeClass(clazz, methodIT2).get(0));
-    Assert.assertTrue(Integer.class == TypeUtil.INSTANCE.getMethodReturnTypeClass(clazz, methodIT2).get(1));
-    Assert.assertTrue(Long.class == TypeUtil.INSTANCE.getMethodReturnTypeClass(clazz, methodCT1).get(0));
-    Assert.assertTrue(List.class == TypeUtil.INSTANCE.getMethodReturnTypeClass(clazz, methodCT2).get(0));
-    Assert.assertTrue(Short.class == TypeUtil.INSTANCE.getMethodReturnTypeClass(clazz, methodCT2).get(1));
+    Assert.assertEquals(String.class.getName(), TypeUtil.getMethodReturnTypeClass(clazz, methodString).toString());
+    Assert.assertEquals(Object.class.getName(), TypeUtil.getMethodReturnTypeClass(clazz, methodIT1).toString());
+    Assert.assertEquals("java.util.List<java.lang.Integer>", TypeUtil.getMethodReturnTypeClass(clazz, methodIT2).toString());
+    Assert.assertEquals(Long.class.getName(), TypeUtil.getMethodReturnTypeClass(clazz, methodCT1).toString());
+    Assert.assertEquals("java.util.List<java.lang.Short>", TypeUtil.getMethodReturnTypeClass(clazz, methodCT2).toString());
   }
   
-  @Ignore
   @Test
   public void testgetMethodParameterTypeClass() {
     Class<?> clazz = CT3.class;
@@ -80,13 +96,11 @@ public class TypeUtilTest {
     Method methodIT2 = ReflectUtil.getDeclaredMethod(clazz, "getIT2", List.class, Integer.class);
     Method methodCT1 = ReflectUtil.getDeclaredMethod(clazz, "getCT1", Long.class);
     Method methodCT2 = ReflectUtil.getDeclaredMethod(clazz, "getCT2", String.class, List.class);
-    Assert.assertTrue(String.class == TypeUtil.INSTANCE.getMethodParameterTypeClass(clazz, methodString, 0).get(0));
-    Assert.assertTrue(Object.class == TypeUtil.INSTANCE.getMethodParameterTypeClass(clazz, methodIT1, 0).get(0));
-    Assert.assertTrue(List.class == TypeUtil.INSTANCE.getMethodParameterTypeClass(clazz, methodIT2, 0).get(0));
-    Assert.assertTrue(Integer.class == TypeUtil.INSTANCE.getMethodParameterTypeClass(clazz, methodIT2, 0).get(1));
-    Assert.assertTrue(Long.class == TypeUtil.INSTANCE.getMethodParameterTypeClass(clazz, methodCT1, 0).get(0));
-    Assert.assertTrue(List.class == TypeUtil.INSTANCE.getMethodParameterTypeClass(clazz, methodCT2, 1).get(0));
-    Assert.assertTrue(Short.class == TypeUtil.INSTANCE.getMethodParameterTypeClass(clazz, methodCT2, 1).get(1));
+    Assert.assertEquals(String.class.getName(), TypeUtil.getMethodParameterTypeClass(clazz, methodString, 0).toString());
+    Assert.assertEquals(Object.class.getName(), TypeUtil.getMethodParameterTypeClass(clazz, methodIT1, 0).toString());
+    Assert.assertEquals("java.util.List<java.lang.Integer>", TypeUtil.getMethodParameterTypeClass(clazz, methodIT2, 0).toString());
+    Assert.assertEquals(Long.class.getName(), TypeUtil.getMethodParameterTypeClass(clazz, methodCT1, 0).toString());
+    Assert.assertEquals("java.util.List<java.lang.Short>", TypeUtil.getMethodParameterTypeClass(clazz, methodCT2, 1).toString());
   }
   
   interface IT1<E> {
@@ -137,5 +151,10 @@ public class TypeUtilTest {
     public String getString(String s) {
       return null;
     }
+  }
+  
+  class GT {
+    List<List<Map<String, Map<String, String>>>> clist;
+    Map<String, Map<String, Map<String, String>>> triMap;
   }
 }
