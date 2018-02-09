dbc8533445c86bda68670ee85867bcdae60ca679
Jinglei.Gu <jinglei.gu@oracle.com>
2018-02-10 02:28:29 +0800

    +ReflectUtil.getPackageName()

--- commons/src/main/java/com/elminster/common/util/ReflectUtil.java
+++ commons/src/main/java/com/elminster/common/util/ReflectUtil.java
@@ -3,9 +3,11 @@ package com.elminster.common.util;
 import java.lang.reflect.Array;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
+import java.lang.reflect.Proxy;
 import java.net.URL;
 import java.security.CodeSource;
 import java.security.ProtectionDomain;
@@ -675,5 +677,38 @@ public abstract class ReflectUtil {
     }
     return result;
   }
+  
+  /**
+   * Returns the package name of {@code clazz} according to the Java Language Specification (section
+   * 6.7). Unlike {@link Class#getPackage}, this method only parses the class name, without
+   * attempting to define the {@link Package} and hence load files.
+   */
+  public static String getPackageName(Class<?> clazz) {
+    return getPackageName(clazz.getName());
+  }
 
+  /**
+   * Returns the package name of {@code classFullName} according to the Java Language Specification
+   * (section 6.7). Unlike {@link Class#getPackage}, this method only parses the class name, without
+   * attempting to define the {@link Package} and hence load files.
+   */
+  public static String getPackageName(String classFullName) {
+    int lastDot = classFullName.lastIndexOf('.');
+    return (lastDot < 0) ? "" : classFullName.substring(0, lastDot);
+  }
+
+
+  /**
+   * Returns a proxy instance that implements {@code interfaceType} by dispatching method invocations to {@code handler}. The class loader of {@code interfaceType} will be used to
+   * define the proxy class. To implement multiple interfaces or specify a class loader, use {@link Proxy#newProxyInstance}.
+   *
+   * @throws IllegalArgumentException
+   *           if {@code interfaceType} does not specify the type of a Java interface
+   */
+  public static <T> T newProxy(Class<T> interfaceType, InvocationHandler handler) {
+    Assert.notNull(handler);
+    Assert.isTrue(interfaceType.isInterface(), "%s is not an interface", interfaceType);
+    Object object = Proxy.newProxyInstance(interfaceType.getClassLoader(), new Class<?>[] { interfaceType }, handler);
+    return interfaceType.cast(object);
+  }
 }
